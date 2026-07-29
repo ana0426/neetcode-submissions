2e0136a32079cd12b780684f5e2ff125e2f15b26
ana0426 <ana.0426.ai@gmail.com>
2026-07-29 09:26:20 +0200

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prehash = {}
+
+        for i , n in enumerate(nums):
+            diff = target - n
+            if diff in prehash:
+                return [prehash[diff], i]
+            prehash[n] = i
+        
